84e7a3f39161096493dd93137eb9ab96d6dafa57
Samannoy Bhattacharjee <82638503+SamannoyB@users.noreply.github.com>
2025-01-19 23:38:07 +0530

    Update test.js

--- test.js
+++ test.js
@@ -4,4 +4,4 @@ plugdb.setup({ path: 'D:/'});
 plugdb.initializeCluster('kaka');
 plugdb.createNewDocument('kaka', 'users');
 plugdb.addData('kaka', 'users', { id: 'foo', username: 'foobar'});
-plugdb.modifyDocument('kaka', 'users', 'name', 'nouka', 'noku')
+plugdb.modifyDocument('kaka', 'users', 'name', 'sb', 'ssb');
